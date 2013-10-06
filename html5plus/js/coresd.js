4798f0362ff00da760bbb6547396dd4926340c62
svnlabs <svnlabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-06 15:36:34 +0000

    jQuery issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/html5-mp3-player-with-playlist/trunk@783505 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html5plus/js/coresd.js
+++ html5plus/js/coresd.js
@@ -1,16 +1,16 @@
 looptrack = 0;
-      $(function() { 
+      jQuery(function() { 
         // Setup the player to autoplay the next track
         var a = audiojs.createAll({
           trackEnded: function() {
-            var next = looptrack==0 ? $('ol li.playing').next().next() : $('ol li.playing');
-            if (!next.length) next = $('ol li').first();
+            var next = looptrack==0 ? jQuery('ol li.playing').next().next() : jQuery('ol li.playing');
+            if (!next.length) next = jQuery('ol li').first();
             next.addClass('playing').siblings().removeClass('playing');
-            audio.load($('a', next).attr('data-src'));
+            audio.load(jQuery('a', next).attr('data-src'));
 			audio.play();
 			
-			$('#current-track'+id).html($('a', next).attr('title'));
-			$('#current-play-pic'+id).attr('src', $('a', next).attr('name'));
+			jQuery('#current-track'+id).html(jQuery('a', next).attr('title'));
+			jQuery('#current-play-pic'+id).attr('src', jQuery('a', next).attr('name'));
 			
           }
         });
@@ -18,71 +18,71 @@ looptrack = 0;
         
         // Load in the first track
         var audio = a[0];
-            first = $('ol li a').attr('data-src'); // previous first = $('ol a').attr('data-src');
-        $('ol li').first().addClass('playing');
+            first = jQuery('ol li a').attr('data-src'); // previous first = jQuery('ol a').attr('data-src');
+        jQuery('ol li').first().addClass('playing');
         audio.load(first);
-		$('#current-track'+id).html($('ol li a').first().attr('title'));
-		$('#current-play-pic'+id).attr('src',$('ol li a').first().attr('name'));
+		jQuery('#current-track'+id).html(jQuery('ol li a').first().attr('title'));
+		jQuery('#current-play-pic'+id).attr('src',jQuery('ol li a').first().attr('name'));
 		
 
         // Load in a track on click
-        $('ol li').click(function(e) {
+        jQuery('ol li').click(function(e) {
           e.preventDefault();
-          $(this).addClass('playing').siblings().removeClass('playing');
-          audio.load($('a', this).attr('data-src'));
+          jQuery(this).addClass('playing').siblings().removeClass('playing');
+          audio.load(jQuery('a', this).attr('data-src'));
           audio.play();
         });
 		
 		// change current track name & image
-		$('.track-name').click(function(e) {
-          $('#current-track'+id).html($(this).attr('title'));
-		  $('#current-play-pic'+id).attr('src',$(this).attr('name'));
+		jQuery('.track-name').click(function(e) {
+          jQuery('#current-track'+id).html(jQuery(this).attr('title'));
+		  jQuery('#current-play-pic'+id).attr('src',jQuery(this).attr('name'));
         });
 		
 		// mouse click events for track change		
-		$('#next_track'+id).click(function() { 
-			var next = $('.playing').next().next(); // previous var next = $('.playing').next();
-			if (!next.length) next = $('.Palylist01').first();
-			if($('a', next).attr('title')!=null){
-				$('#current-track'+id).html($('a', next).attr('title'));
-				$('#current-play-pic'+id).attr('src', $('a', next).attr('name'));
+		jQuery('#next_track'+id).click(function() { 
+			var next = jQuery('.playing').next().next(); // previous var next = jQuery('.playing').next();
+			if (!next.length) next = jQuery('.Palylist01').first();
+			if(jQuery('a', next).attr('title')!=null){
+				jQuery('#current-track'+id).html(jQuery('a', next).attr('title'));
+				jQuery('#current-play-pic'+id).attr('src', jQuery('a', next).attr('name'));
 				
-			//$('#scrollbar1').tinyscrollbar_update(60);	
+			//jQuery('#scrollbar1').tinyscrollbar_update(60);	
 			}
 			next.click();
 			
 		
 		});	
 				
-		$('#prev_track'+id).click(function() { 
-			var prev = $('.playing').prev().prev(); // previous var prev = $('.playing').prev();
-            if (!prev.length) prev = $('.Palylist01').last();
-			if($('a', prev).attr('title')!=null){
-				$('#current-track'+id).html($('a', prev).attr('title'));
-				$('#current-play-pic'+id).attr('src', $('a', prev).attr('name'));
+		jQuery('#prev_track'+id).click(function() { 
+			var prev = jQuery('.playing').prev().prev(); // previous var prev = jQuery('.playing').prev();
+            if (!prev.length) prev = jQuery('.Palylist01').last();
+			if(jQuery('a', prev).attr('title')!=null){
+				jQuery('#current-track'+id).html(jQuery('a', prev).attr('title'));
+				jQuery('#current-play-pic'+id).attr('src', jQuery('a', prev).attr('name'));
 				
-			//$('#scrollbar1').tinyscrollbar_update(0);	
+			//jQuery('#scrollbar1').tinyscrollbar_update(0);	
 			}
             prev.click();
 
 			
 		});	
 		
-		$('#play-pause'+id).click(function() { 
+		jQuery('#play-pause'+id).click(function() { 
 			 audio.playPause();
 		});	
 		
 		// loop track 
-		$('#loop-track'+id).click(function(){
-	      $('#loop-track-box'+id).hide();
-		  $('#noloop-track-box'+id).show();
+		jQuery('#loop-track'+id).click(function(){
+	      jQuery('#loop-track-box'+id).hide();
+		  jQuery('#noloop-track-box'+id).show();
 		  //audio.settings.loop = true;
 		  looptrack = 1;
 		});
 		
-		$('#noloop-track'+id).click(function(){
-	      $('#loop-track-box'+id).show();
-		  $('#noloop-track-box'+id).hide();
+		jQuery('#noloop-track'+id).click(function(){
+	      jQuery('#loop-track-box'+id).show();
+		  jQuery('#noloop-track-box'+id).hide();
 		 // audio.settings.loop = false;
 		 looptrack = 0;
 		});
@@ -90,22 +90,22 @@ looptrack = 0;
 		
 		// shuffle tracks
 		
-		$('#shuffle-track'+id).click(function(){
-	      $('#shuffle-track-box'+id).hide();
-		  $('#noshuffle-track-box'+id).show();
+		jQuery('#shuffle-track'+id).click(function(){
+	      jQuery('#shuffle-track-box'+id).hide();
+		  jQuery('#noshuffle-track-box'+id).show();
 		  
 		  
-		$(".track-name").each(function (idx, el) { 
+		jQuery(".track-name").each(function (idx, el) { 
 		
 		  //alert( "this is element " + idx);
 		  
-		  //var idd = $(this).attr('id');   
+		  //var idd = jQuery(this).attr('id');   
 		  
-		  //console.log( idd+' data-src= ' + $(this).attr('data-src') );
+		  //console.log( idd+' data-src= ' + jQuery(this).attr('data-src') );
 		  
-		  $('#source'+id+idx+'download').attr('href', $(this).attr('data-src'))
+		  jQuery('#source'+id+idx+'download').attr('href', jQuery(this).attr('data-src'))
 		  
-		  //console.log( idx+' download.src= ' + $('#source'+idd+'download').attr('href') );
+		  //console.log( idx+' download.src= ' + jQuery('#source'+idd+'download').attr('href') );
 		  
 		
 		});
@@ -113,15 +113,15 @@ looptrack = 0;
 		  
 		});
 		
-		$('#noshuffle-track'+id).click(function(){
-	      $('#shuffle-track-box'+id).show();
-		  $('#noshuffle-track-box'+id).hide();
+		jQuery('#noshuffle-track'+id).click(function(){
+	      jQuery('#shuffle-track-box'+id).show();
+		  jQuery('#noshuffle-track-box'+id).hide();
 		 // audio.settings.loop = false;
 		// shuffletrack = 0;
 		});
 		
 	   
-	    var scrubber1 = $('#Player-progressbar01'+id);
+	    var scrubber1 = jQuery('#Player-progressbar01'+id);
 		
 		leftPos = function(elem) {
             var curleft = 0;
@@ -131,7 +131,7 @@ looptrack = 0;
             return curleft;
           };
 	     
-		 $('#Player-progressbar01'+id).click(function(e){
+		 jQuery('#Player-progressbar01'+id).click(function(e){
 		 var relativeLeft = e.clientX - leftPos(this);
 		 
 		 //alert(e.clientX);
@@ -144,17 +144,17 @@ looptrack = 0;
 		
 		
         // Keyboard shortcuts
-        $(document).keydown(function(e) {
+        jQuery(document).keydown(function(e) {
           var unicode = e.charCode ? e.charCode : e.keyCode;
              // right arrow
           if (unicode == 39) {
-            var next = $('li.playing').next().next();
-            if (!next.length) next = $('ol li').first();
+            var next = jQuery('li.playing').next().next();
+            if (!next.length) next = jQuery('ol li').first();
             next.click();
             // back arrow
           } else if (unicode == 37) {
-            var prev = $('li.playing').prev().prev();
-            if (!prev.length) prev = $('ol li').last();
+            var prev = jQuery('li.playing').prev().prev();
+            if (!prev.length) prev = jQuery('ol li').last();
             prev.click();
             // spacebar
           } else if (unicode == 32) {
