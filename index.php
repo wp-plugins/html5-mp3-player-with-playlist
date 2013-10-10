5cebff91d0b08b876867e2be5ac43b698f32f2d8
svnlabs <svnlabs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-10 12:51:47 +0000

    html5mp3onUpgrade
    git-svn-id: https://plugins.svn.wordpress.org/html5-mp3-player-with-playlist/trunk@785881 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -350,12 +350,15 @@ function html5mp3onUpgrade() {
 global $wpdb;
 $table_name_xml = "xml";
 
-if($wpdb->get_var("SHOW TABLES LIKE '$table_name_xml'") != $table_name_xml  ) 
+$wpdb->get_var("SHOW TABLES LIKE '$table_name_xml'");
+
+if($wpdb->get_var("SHOW TABLES LIKE '$table_name_xml'") == $table_name_xml  ) 
 {
+
 $sql = "ALTER TABLE  `xml` ADD  `content` TEXT NOT NULL AFTER  `xml` ";
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
+dbDelta($sql);
 
 }
 
